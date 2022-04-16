95b453ddf80951e7ff145fdd9c6cab1d913d3740
leopassosleite <leopassos.leite4@gmail.com>
2022-04-16 00:21:22 -0300

    update

--- frontend/src/pages/Home/index.js
+++ frontend/src/pages/Home/index.js
@@ -1,9 +1,15 @@
 import { Link } from "react-router-dom"
+import './styles.css'
+
+const product = {
+    img: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcTsCZ27bBHg_N75iWfrJKN2OusK0z0Q6CJfkQ&usqp=CAU"
+}
 
 function Home() {
     return (
-        <div>
-            <h1>OlÁ</h1>
+        <div className="ju-bonde">
+            <h1>OInnnn ROGINHO</h1>
+            <img src={product.img} alt="image"></img>
             <Link to="/clientes"><button>clientes</button></Link>
         </div>
     )
