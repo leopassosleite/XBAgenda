5d625235fafe07cbb2bacfee451bfea09590d8e7
leopassosleite <leopassos.leite4@gmail.com>
2022-04-15 23:48:04 -0300

    Project Created

---
+++ backend/src/main/java/com/xbarts/xbcritic/dto/ClientDTO.java
@@ -0,0 +1,81 @@
+package com.xbarts.xbcritic.dto;
+
+import com.xbarts.xbcritic.entities.Client;
+
+public class ClientDTO {
+	private Long id;
+	private String name;
+	private String company;
+	private String email;
+	private String phone;
+	private String category;
+	
+	public ClientDTO() {
+	}
+
+	public ClientDTO(Long id, String name, String company, String email, String phone, String category) {
+		this.id = id;
+		this.name = name;
+		this.company = company;
+		this.email = email;
+		this.phone = phone;
+		this.category = category;
+	}
+	
+	public ClientDTO(Client client) {
+		id = client.getId();
+		name = client.getName();
+		company = client.getCompany();
+		email = client.getEmail();
+		phone = client.getPhone();
+		category = client.getCategory();
+	}
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+}
---
+++ backend/src/main/java/com/xbarts/xbcritic/entities/Client.java
@@ -0,0 +1,113 @@
+package com.xbarts.xbcritic.entities;
+
+import java.util.Objects;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "tb_client")
+public class Client {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+	
+	@Column(name = "name")
+	private String name;
+	
+	@Column(name = "company")
+	private String company;
+	
+	@Column(name = "email")
+	private String email;
+	
+	@Column(name = "phone")
+	private String phone;
+	
+	@Column(name = "category")
+	private String category;
+	
+	public Client() {
+	}
+	
+	public Client(Long id, String name, String company, String email, String phone, String category) {
+		this.id = id;
+		this.name = name;
+		this.company = company;
+		this.email = email;
+		this.phone = phone;
+		this.category = category;
+	}
+
+
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public String getCompany() {
+		return company;
+	}
+
+	public void setCompany(String company) {
+		this.company = company;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getPhone() {
+		return phone;
+	}
+
+	public void setPhone(String phone) {
+		this.phone = phone;
+	}
+	
+	public String getCategory() {
+		return category;
+	}
+
+	public void setCategory(String category) {
+		this.category = category;
+	}
+
+	@Override
+	public int hashCode() {
+		return Objects.hash(id);
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Client other = (Client) obj;
+		return Objects.equals(id, other.id);
+	}
+}
